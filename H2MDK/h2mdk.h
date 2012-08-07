852bf462c81ba430ef5c9103d0b29cbf4f887b40
matthew venn <matt@mattvenn.net>
2012-08-07 12:23:16 +0100

    added hardware version. removed bandgap stuff

--- H2MDK/h2mdk.h
+++ H2MDK/h2mdk.h
@@ -21,50 +21,31 @@
 #ifndef h2mdk_h
 #define h2mdk_h
 
-//version types
+#if ARDUINO >= 100
+  #include <Arduino.h>
+#else
+  #include <WProgram.h>
+#endif
+
+//stack version types
 #define V1_5W 1
 #define V3W 2
 #define V12W 3
 #define V30W 4
+//hardware version types
+#define V1 1
+#define V2 2
 
-//timing defs
-#define PREPURGE 200 //to ensure we don't purge while shorting!
-#define ELECT_INTERVAL 100 //how often to sample electrical 
-#define BLINK_INTERVAL 500 //how often to sample electrical 
-
-#define FILTER 0.8 //coefficient for LPF on current sense
-//band gap can be measured like this: http://bit.ly/KQmkma
-#define DEFAULTBANDGAP 1100 //for better ADC accuracy, pass your bandgap to constructor
-#define CAP_V 2900 //this will need a potential divider
-
-//digital IO pins
-#define PURGE 3
-#define LOAD 4  // 3w can disconnect load
-#define OSC 4   // 12-30w needs oscillator for mosfet charge pump
-#define SHORT 5
-#define STATUS_LED 6
-
-//analog pins
-#define VOLTAGE_SENSE A1
-#define CURRENT_SENSE A2
-#define CAP_V_SENSE A3
-
-const float _aRef = 3300.0;
 class h2mdk
 {
   public:
-    h2mdk(int version, int bandGap);
-    h2mdk(int version);
     void poll();
     void status();
     float getVoltage();
-    float getSupplyMV();
     float getCurrent();
     void start();
   private:
-    void _init(int version, int bandGap );
     int _vccRead();
-    int _version;
     bool _ledstate;
     bool _ni(bool);
     float _current;
@@ -74,23 +55,306 @@ class h2mdk
     void _updateElect();
     void _blink();
     void _checkCaps();
-    void _setupTimings(int version);
-    void _overloadCutout();
-    //time vars
-    unsigned int _shortCircuitInterval;
-    unsigned int _shortTime;
-    unsigned long _purgeInterval;
-    unsigned int _purgeTime;
-    //for doing the timing
+
+//timing defs
+    static const int PREPURGE = 200; //to ensure we don't purge while shorting!
+    static const int ELECT_INTERVAL = 400; //how often to sample electrical
+    static const int BLINK_INTERVAL = 500; //how often to sample electrical
+
+    static const float FILTER = 0.9; //coefficient for LPF on current sense
+//band gap can be measured like this: http://bit.ly/KQmkma
+    static const int DEFAULTBANDGAP = 1100; //for better ADC accuracy, pass your bandgap to constructor
+    static const int CAP_V = 800;
+
+//digital IO pins
+  #if( _hardware == V1 )
+    static const int PURGE = 2;
+    static const int LOAD = 3;  // 3w can disconnect load
+    static const int OSC = 3;   // 12-30w needs oscillator for mosfet charge pump
+    static const int SHORT = 4;
+    static const int STATUS_LED = 5;
+  #elif( _hardware == V2 )
+
+  #endif
+//analog pins
+    static const int VOLTAGE_SENSE = A1;
+    static const int CURRENT_SENSE = A2;
+    static const int CAP_V_SENSE = A3;
+
+/*
+1.5W stack (info from Horizon)
+Purge: 100ms every 4 mins
+Short circuit: 100ms every 10s
+
+H-12 (no info from Horizon as they don't have H-12 controller)
+Purge: Shall we assume 50ms every 25s (because H2 consumption is less)
+Short circuit: 100ms every 10s (assuming same as other stacks)
+
+H-30 (info from Horizon)
+Purge: 50ms every 10s
+Short circuit: 100ms every 10s
+*/
+//time vars
+  // all in ms
+  #if( _version == V1_5W )
+    static const unsigned int _shortCircuitInterval = 10000;
+    static const unsigned int _shortTime = 100;
+    static const unsigned long _purgeInterval = 60000; //240000;
+    static const unsigned int _purgeTime = 100;
+
+  #elif( _version == V3W )
+    static const unsigned int _shortCircuitInterval = 10000;
+    static const unsigned int _shortTime = 100;
+    static const unsigned long _purgeInterval = 60000; //240000;
+    static const unsigned int _purgeTime = 100;
+
+  #elif( _version == V12W )
+    static const unsigned int _shortCircuitInterval = 10000;
+    static const unsigned int _shortTime = 100;
+    static const unsigned long _purgeInterval = 25000;
+    static const unsigned int _purgeTime = 50;
+
+  #elif( _version == V30W )
+    static const unsigned int _shortCircuitInterval = 10000;
+    static const unsigned int _shortTime = 100;
+    static const unsigned long _purgeInterval = 10000;
+    static const unsigned int _purgeTime = 50;
+  #else
+  #endif
+
+//for doing the timing
     unsigned int _shortCircuitTimer;
     unsigned int _electTimer;
     unsigned long _purgeTimer;
     unsigned int _statusTimer;
     unsigned long _lastPoll;
     float _filteredRawCurrent;
-    int _bandGap;
-    float _cutoutVoltage;
-    float _supplyMV;
+  #ifdef BANDGAP
+    static const int _bandGap = BANDGAP;
+  #else
+    static const int _bandGap = DEFAULTBANDGAP;
+  #endif
 };
 
+
+
+
+
+void h2mdk::start()
+{
+  _shortCircuitTimer = 0;
+  _purgeTimer = 0;
+  _electTimer = 0;
+  _statusTimer = 0;
+
+  _filteredRawCurrent = 512; //should be at zero A (ie 2.5v) to start with
+
+  pinMode(STATUS_LED, OUTPUT);
+
+  if( _version == V3W || _version == V1_5W )
+  {
+    pinMode( LOAD, OUTPUT );
+    //connect the load to charge the caps
+    digitalWrite( LOAD, _ni(HIGH) );
+  }
+  else if( _version == V12W || _version == V30W )
+  {
+  //charge pump waveform
+    analogWrite( OSC, 128 );
+  }
+
+  pinMode( SHORT, OUTPUT );
+  digitalWrite( SHORT, _ni(LOW) );
+  pinMode( PURGE, OUTPUT );
+  digitalWrite( PURGE, _ni(LOW) );
+
+  Serial.println( "ArcolaEnergy fuel cell controller for "
+  #if( _version == V1_5W )
+    "1.5W"
+  #elif( _version == V3W )
+    "3W"
+  #elif( _version == V12W )
+    "12W"
+  #elif( _version == V30W )
+    "30W"
+  #else
+  #endif
+  );
+
+  Serial.print("Short-circuit: ");
+  Serial.print(_shortTime);
+  Serial.print("ms every ");
+  Serial.print(_shortCircuitInterval / 1000);
+  Serial.println(" s");
+
+  Serial.print("Purge: ");
+  Serial.print(_purgeTime);
+  Serial.print("ms every ");
+  Serial.print( _purgeInterval / 1000);
+  Serial.println(" s");
+
+  //wait for cap to charge
+  Serial.println( "waiting for caps to charge" );
+  _checkCaps();
+}
+float h2mdk::getVoltage()
+{
+  return _voltage;
+}
+
+float h2mdk::getCurrent()
+{
+  return _current;
+}
+
+void h2mdk::status()
+{
+  Serial.print( _voltage );
+  Serial.print( "V, " );
+  Serial.print( _current );
+  Serial.println( "A" );
+}
+
+void h2mdk::poll()
+{
+  int interval = millis() - _lastPoll;
+  _lastPoll = millis();
+
+  _electTimer += interval;
+  _statusTimer += interval;
+  _purgeTimer += interval;
+  _shortCircuitTimer += interval;
+
+  _lastPoll = millis();
+
+  if( _statusTimer > BLINK_INTERVAL )
+  {
+    _blink();
+    status();
+    _statusTimer = 0;
+  }
+  if( _electTimer > ELECT_INTERVAL )
+  {
+    _updateElect();
+    _electTimer = 0;
+  }
+  if( _shortCircuitTimer > _shortCircuitInterval )
+  {
+    _shortCircuit();
+    _shortCircuitTimer = 0;
+  }
+  if( _purgeTimer > _purgeInterval )
+  {
+    _purge();
+    _purgeTimer = 0;
+  }
+
+}
+
+void h2mdk::_checkCaps()
+{
+  while( analogRead( CAP_V_SENSE ) < CAP_V )
+  {
+    Serial.println( analogRead( CAP_V_SENSE ) );
+    _blink();
+    delay(200);
+  }
+  Serial.println( "CHARGED" );
+}
+
+void h2mdk::_updateElect()
+{
+  //read this to get more accurate ADC readings. As load increases, supplyMV drops. So we read the supplyMV before doing our other measurements.
+  float supplyMV = _vccRead();
+
+  //voltage
+  float rawStackV = analogRead(VOLTAGE_SENSE );
+  float stackVoltage;
+  if( _version == V3W || _version == V1_5W )
+    stackVoltage = (supplyMV/1024*rawStackV);
+  //for 12 and 30W
+  else if( _version == V12W || _version == V30W )
+    stackVoltage = (326.00/100.00)*(supplyMV/1024*rawStackV); // R1=226k R2=100k
+
+  _voltage = stackVoltage/1000;
+
+  //current
+  //100 times average of current.
+  _filteredRawCurrent = _filteredRawCurrent * FILTER  + ( 1 - FILTER ) * analogRead(CURRENT_SENSE);
+  float currentMV = (supplyMV / 1024 ) * _filteredRawCurrent;
+  if( _version == V3W || _version == V1_5W )
+    //current sense chip is powered from 5v regulator
+    _current = ( currentMV - 5040 / 2 ) / 185; //185mv per amp
+  else if( _version == V12W || _version == V30W )
+    //current sense chip is powered by arduino supply
+    _current = ( currentMV - supplyMV / 2 ) / 185; //185mv per amp
+
+}
+
+
+void h2mdk::_purge()
+{
+  delay( PREPURGE );
+  Serial.println("PURGE");
+  digitalWrite( PURGE, _ni(HIGH) );
+  delay( _purgeTime);
+  digitalWrite( PURGE, _ni(LOW) );
+}
+
+void h2mdk::_shortCircuit()
+{
+  if (analogRead( CAP_V_SENSE ) < CAP_V)
+  {
+    Serial.println("SKIPPING SHORT-CIRCUIT AS SUPERCAP VOLTAGE TOO LOW");
+    return;
+  }
+
+  if( _version == V3W || _version == V1_5W )
+    //disconnect load
+    digitalWrite( LOAD, _ni(LOW) );
+
+  //short circuit
+  digitalWrite( SHORT, _ni(HIGH) );
+  Serial.println("SHORT-CIRCUIT");
+  delay(_shortTime);
+  digitalWrite( SHORT, _ni(LOW) );
+
+  if( _version == V3W || _version == V1_5W )
+    //reconnect
+    digitalWrite( LOAD, _ni(HIGH) );
+}
+
+//utility to invert the mosfet pins for the 12 and 30W control boards
+inline bool h2mdk::_ni(bool state)
+{
+  if( _version == V3W || _version == V1_5W )
+    return state;
+  if( _version == V12W || _version == V30W )
+    return ! state;
+}
+
+//blink the status led
+void h2mdk::_blink()
+{
+  digitalWrite(STATUS_LED, _ledstate );
+  _ledstate = ! _ledstate;
+}
+
+
+//measure VCC
+//needs accurate bandgap
+//thanks to JeeLabs for this
+//http://jeelabs.org/2012/05/04/measuring-vcc-via-the-bandgap/
+int h2mdk::_vccRead()
+{
+  analogRead(6);
+  bitSet(ADMUX,3);
+  delayMicroseconds(550);
+  bitSet(ADCSRA, ADSC );
+  while( bit_is_set(ADCSRA,ADSC));
+  word x = ADC;
+  return x ? (_bandGap * 1023L) / x : -1;
+}
+
 #endif
+
